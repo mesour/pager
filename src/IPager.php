55999888c9269a54ec857d4ff7bc96bc66ac0a9b
mesour <matous.nemec@mesour.com>
2015-05-24 17:55:50 +0200

    Some changes

---
+++ src/IPager.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Mesour Pager Component
+ *
+ * @license LGPL-3.0 and BSD-3-Clause
+ * @copyright (c) 2015 Matous Nemec <matous.nemec@mesour.com>
+ */
+
+namespace Mesour\UI;
+
+use Mesour\Components;
+use Mesour\Pager\Paginator;
+
+/**
+ * @author mesour <matous.nemec@mesour.com>
+ * @package Mesour Pager Component
+ */
+interface IPager extends Components\IComponent
+{
+
+    public function setCounts($total_count, $limit);
+
+    /**
+     * @return Paginator
+     */
+    public function getPaginator();
+
+    public function getWrapperPrototype();
+
+    public function getControlPrototype();
+
+    public function create($data = array());
+
+}
